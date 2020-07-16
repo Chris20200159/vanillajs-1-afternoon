ddfa89cb57f38abbf5a14d554bb33d4823cd319f
Chris Vega <catalina@MacBook-Pro.local>
2020-07-15 22:49:25 -0700

    updated project

--- Part2/index.js
+++ Part2/index.js
@@ -1,1 +1,23 @@
 console.log(House Always Wins!)
+
+const idInput = document.getElementById('idInput');
+console.log(idInput)
+
+const colorInput = document.getElementById('colorInput');
+console.log(colorInput)
+
+function setCard () {
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
+}
+console.log(card)
+
+Function reset() {
+
+  const button = document.createElement("button");
+  button.innerHTML = "Reset";  
+
+  button.addEventListener("click", function () {
+    alert("Board Reset");
+  });
+}
